d62b94e4170467b61301fa541998f3d657e15059
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2025-11-17 02:44:32 +0000

    add more env tests

--- internal/locator/env_test.go
+++ internal/locator/env_test.go
@@ -3,25 +3,47 @@ package locator_test
 import (
 	"testing"
 
+	"github.com/peter-bread/gamon3/internal/data"
 	"github.com/peter-bread/gamon3/internal/locator"
 )
 
 func TestEnvAccount(t *testing.T) {
 	tests := []struct {
 		name  string // description of this test case
+		env   string
+		isSet bool
 		want  string
 		want2 bool
 	}{
-		// TODO: Add test cases.
+		{
+			name:  "false if env var is unset",
+			isSet: false,
+			want2: false,
+		},
+		{
+			name:  "true if env var is set but empty",
+			isSet: true,
+			want2: true,
+		},
+		{
+			name:  "true if env var is set and non-empty",
+			env:   "hello",
+			isSet: true,
+			want:  "hello",
+			want2: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			if tt.isSet {
+				t.Setenv(data.EnvVarAccount, tt.env)
+			}
+
 			got, got2 := locator.EnvAccount()
-			// TODO: update the condition below to compare got with tt.want.
-			if true {
+			if got != tt.want {
 				t.Errorf("EnvAccount() = %v, want %v", got, tt.want)
 			}
-			if true {
+			if got2 != tt.want2 {
 				t.Errorf("EnvAccount() = %v, want %v", got2, tt.want2)
 			}
 		})
