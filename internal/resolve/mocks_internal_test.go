7c2bab4f588be569ff81b4dc4004a326b074dae3
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2025-11-17 18:14:27 +0000

    refactor(resolve): define mock structs in their own file (white-box)

---
+++ internal/resolve/mocks_internal_test.go
@@ -0,0 +1,9 @@
+package resolve
+
+type mockGhHosts struct {
+	current string
+	users   []string
+}
+
+func (m mockGhHosts) CurrentUser() string { return m.current }
+func (m mockGhHosts) AllUsers() []string  { return m.users }
