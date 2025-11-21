8a6ad5e9e4ccad481f0a7c87973b40ab8d645065
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2025-11-21 23:16:26 +0000

    docs(data): adds comments to data

--- internal/data/constants.go
+++ internal/data/constants.go
@@ -1,3 +1,6 @@
+// Package data stores information needed across this project.
 package data
 
+// EnvVarAccount represents the name of enviornment variable that can be used
+// to set the desired GitHub account.
 const EnvVarAccount = "GAMON3_ACCOUNT"
